3c21533a29ae8d409c788a48bdee28d887076285
Holden Steinhauser <holdenstein17@gmail.com>
2019-11-04 16:44:22 -0600

    Landing Page Created

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+</head>
+<body>
+
+<?php include 'view/components/nav.php' ?>
+
+<header>
+    <div id="title-block">
+        <h1 id="title">LOOKING FOR TEAM</h1>
+        <h3 id="subtitle">Where Players and Teams Meet</h3>
+        <button id="get-started">Get Started</button>
+    </div>
+</header>
+
+<section id="games">
+    <h1 class="section-header">Games</h1>
+
+    <ul id="games-list">
+        <li class="game-item">
+            <img src="assets/team-logos/overwatch.png" alt="Overwatch Logo" />
+        </li>
+        <li class="game-item">
+            <img src="assets/team-logos/csgo.png" alt="CSGO Logo">
+        </li>
+        <li class="game-item">
+            <img src="assets/team-logos/apex.png" alt="Apex Legends Logo">
+        </li>
+        <li class="game-item">
+            <img src="assets/team-logos/league-of-legends.png" alt="League of Legends Logo">
+        </li>
+    </ul>
+</section>
+
+
+    
+</body>
+</html>
---
+++ view/components/nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <h1 id="logo">LFT</h1>
+
+    <ul id="nav-list">
+        <li class="nav-item">Games</li>
+        <li class="nav-item">About</li>
+        <li class="nav-item">Contact</li>
+        <li class="nav-item" id="important">Login</li>
+    </ul>
+</nav>
